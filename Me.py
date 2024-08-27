acdfaf803ae5a73f9651dde9b306b86700107260
buntidem00 <buntidem001@warren.k12.in.us>
2024-08-27 08:36:35 -0400

    intitial commit

---
+++ Me.py
@@ -0,0 +1,13 @@
+F_text = input("Enter the temp in degrees F: ")
+print(name, "wants to meet", droids, "droids and ", wookiees, "Wookiees!")
+F = float(F_text)
+C = 5 * (f - 32) / 9
+
+name = input("What is your name: ")
+#INSTRUCTION:  write an input
+# statement that asks for
+# number of droid and store as droids
+#INSTRUCTION:  write an input
+# statement that asks for
+# number of wookies and store as wookiees
+print(name, "wants to meet", droids, "droids and ", wookiees, "Wookiees!")
